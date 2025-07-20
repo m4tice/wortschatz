6b0409eb6c673022bb77bef4d3a4517badf3d5db
m4tice <ductuan2908@gmail.com>
2025-07-20 16:17:43 +0700

    feat: add button dedicated function

--- app/wortschatz/static/modes.js
+++ app/wortschatz/static/modes.js
@@ -1,10 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('btn-logout').addEventListener('click', function(event) {
-    event.preventDefault();
     redirect_logout();
+
+    buttons = document.querySelectorAll('.btn');
+    buttons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            const btnId = String(this.getAttribute('id'));
+            const details = btnId.split('-');
+
+            const mode = details[1];
+            const subMode = details.length > 2 ? details[2] : '';
+            const topic = document.getElementById('card-input-topic').value;
+
+            if (mode.includes('daily') && subMode) {
+                console.log(`Daily button clicked with ${subMode} questions`);
+            }
+            else if (mode.includes('level') && subMode) {
+                console.log(`Level button clicked with ${subMode} questions`);
+            }
+            else if (mode.includes('topic')) {
+                if (topic !== '') {
+                    console.log(`Topic button clicked with topic: ${topic}`);
+                }
+                else {
+                    console.log('Topic button clicked but no topic provided');
+                }
+            }
+            else if (btnId.includes('practice')) {
+                console.log('Practice button clicked');
+            }
+            else {
+                console.log(`Button with ID ${btnId} clicked`);
+            }
+        });
     });
 });
 
 function redirect_logout(){
-    window.location.href = '/auth/logout';
+    document.getElementById('btn-logout').addEventListener('click', function(event) {
+        event.preventDefault();
+        window.location.href = '/auth/logout';
+    });
 }
